c57b3121d0a9bab2236b943f62308ab5aebb47c6
niko <1209424096@qq.com>
2023-04-22 12:48:33 +0800

    OrdersAndAccountBook第一次提交

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4_1订单和账本
+{
+    // 打折账本
+    class DisCountOrder : OrderForm
+    {
+        public float discount;
+        public DisCountOrder(string name, int num, float price,float discount)
+            :base(name,num,price)
+        {
+            this.discount = discount;
+        }
+        public override float GetPrice()
+        {
+            return num * price * discount;
+        }
+    }
+
+    // 满减账本
+    class FullSubOrder : OrderForm
+    {
+        public float full;
+        public float sub;
+        public FullSubOrder(string name, int num, float price,float full,float sub)
+            : base(name, num, price)
+        {
+            this.full = full;
+            this.sub = sub;
+        }
+        public override float GetPrice()
+        {
+            float s = num * price;
+            if(s > full)
+            {
+                s -= sub;
+            }
+            return s;
+        }
+
+    }
+
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderForm o1 = new OrderForm("苹果", 3, 1);
+            OrderForm o2 = new OrderForm("香蕉", 34, 0.5f);
+            OrderForm o3 = new OrderForm("橙子", 1, 90);
+            FullSubOrder o4 = new FullSubOrder("车厘子", 20, 0.3f, 5, 1);
+            DisCountOrder o5 = new DisCountOrder("西瓜", 1, 20, 0.8f);
+
+
+
+            AccountBook book = new AccountBook();
+            book.AddOrderForm(o1);
+            book.AddOrderForm(o2);
+            book.AddOrderForm(o3);
+            book.AddOrderForm(o4);
+            book.AddOrderForm(o5);
+
+            Console.WriteLine($"总价是{book.GetTotalPrice()}");
+        }
+        
+        
+    }
+}
---
+++ 基类.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4_1订单和账本
+{
+    class OrderForm
+    {
+        public string name;
+        public int num;
+        public float price;
+        public OrderForm(string name, int num, float price)
+        {
+            this.name = name;
+            this.num = num;
+            this.price = price;
+        }
+        public virtual float GetPrice()
+        { return num * price; }
+    }
+
+    class AccountBook
+    {
+        List<OrderForm> lists = new List<OrderForm>();
+        public void AddOrderForm(OrderForm o)
+        {
+            lists.Add(o);
+        }
+
+        float sum = 0;
+        public float GetTotalPrice()
+        {
+            foreach (var o in lists)
+            {
+                sum += o.GetPrice();
+            }
+            return sum;
+        }
+    }
+
+}
